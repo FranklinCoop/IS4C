3ec881b956e8f4862c15fabf80fb8f4d59e60bcb
Rowan Oberski <rowan.oberski@gmail.com>
2013-06-25 10:19:00 -0400

    edited totals to display correctly

--- pos/is4c-nf/lib/DisplayLib.php
+++ pos/is4c-nf/lib/DisplayLib.php
@@ -128,7 +128,7 @@ static public function printfooter($readOnly=False) {
 	$label = $modchain[3]->header_content();
 	$ret .= sprintf('<td class="reg" style="%s">%s</td>',$modchain[3]->header_css,$label);
 	$label = $modchain[4]->header_content();
-	$ret .= sprintf('<td class="total" style="%s">%s</td>',$modchain[4]->header_css,$label);
+	$ret .= sprintf('<td class="reg" style="%s">%s</td>',$modchain[4]->header_css,$label);
 	$label = $modchain[5]->header_content();
 	$ret .= sprintf('<td class="total" style="%s">%s</td>',$modchain[5]->header_css,$label);
 	/* 5/11/12
@@ -216,7 +216,9 @@ static public function printfooter($readOnly=False) {
 	$box = $modchain[3]->display_content();
 	$ret .= sprintf('<td class="reg" style="%s">%s</td>',$modchain[3]->display_css,$box);
 	$box = $modchain[4]->display_content();
-	$ret .= sprintf('<td class="total" style="%s">%s</td>',$modchain[4]->display_css,$box);
+	$ret .= sprintf('<td class="reg" style="%s">%s</td>',$modchain[4]->display_css,$box);
+	$box = $modchain[5]->display_content();
+	$ret .= sprintf('<td class="total" style="%s">%s</td>',$modchain[5]->display_css,$box);
 	/* 5/11/12
 	$ret .= "<td class=\"first\">".number_format($dblyousaved,2)."</td>";
 	$ret .= "<td class=\"reg\">".$strperdiscount."</td>";

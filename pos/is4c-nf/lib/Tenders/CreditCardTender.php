fffb5c0117a3e5a689a78b2d3a049763f258d2cf
roberski <rowan.oberski@gmail.com>
2014-07-07 11:43:59 -0400

    For some reason a merge took away one of my stupidity checks.

--- pos/is4c-nf/lib/Tenders/CreditCardTender.php
+++ pos/is4c-nf/lib/Tenders/CreditCardTender.php
@@ -36,6 +36,11 @@ class CreditCardTender extends TenderModule
     {
         global $CORE_LOCAL;
         
+       //force negative entered value when the total is negative.
+        if ($CORE_LOCAL->get("amtdue") <0 && $this->amount >= 0) {
+            $this->amount = -1 * $this->amount;
+        }
+        
         if (($this->amount > ($CORE_LOCAL->get("amtdue") + 0.005)) && $CORE_LOCAL->get("amtdue") >= 0) { 
             return DisplayLib::xboxMsg(_("tender cannot exceed purchase amount"));
         } elseif ((($this->amount < ($CORE_LOCAL->get("amtdue") - 0.005)) || ($this->amount > ($CORE_LOCAL->get("amtdue") + 0.005)))

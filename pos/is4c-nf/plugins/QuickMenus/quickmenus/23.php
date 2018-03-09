4dfaa78184f949765a40a56da83d0feef634361d
Rowan Oberski <rowan.oberski@franklincommunity.coop>
2018-03-09 18:19:38 -0500

    Split the Gift card menu's betweent Sales and Activatoins Made all trasactions prompt for Total.

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/23.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/23.php
@@ -3,6 +3,4 @@
 $my_menu = array(
     "Charge Gift Card" => "DATACAPGD",
     "Check Balance" => "PVDATACAPGD",
-    "Activate Card" => "ACDATACAPG",
-    "Add Value to Card" => "APVDATACAPGD",
 );
---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/24.php
@@ -0,0 +1,7 @@
+<?php
+
+$my_menu = array(
+    "Activate Card" => "ACDATACAPGD",
+    "Add Value to Card" => "AVDATACAPGD",
+);
+

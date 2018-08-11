01798c1416ce789688a93d9a1d4ca8efac00fc65
Rowan Oberski <rowan.oberski@franklincommunity.coop>
2018-08-10 21:36:39 -0400

    Hopefully this fixes the issue

--- fannie/cron/tasks/MassStateTaxFreeOff.php
+++ fannie/cron/tasks/MassStateTaxFreeOff.php
@@ -79,7 +79,7 @@ Server side tax table remains the same.';
             $args = array(0.0625, 1); //asummes sales tax is id 1 change as needed
             $query = 'UPDATE '.$lane['trans'].'.taxrates SET rate = ? WHERE id = ?';
             
-            $prep = $dbc->prepare($query);
+            $prep = $dbc->prepare($query,$lane['trans']);
             $result = $dbc->execute($prep,$args);
             
             

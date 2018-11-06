c282ccb7e7ba57e45d0a9496cfb14c8ecb72b298
roberski <rowan.oberski@gmail.com>
2018-11-06 11:14:39 -0500

    budget sales I apprently didn't inlcude this in the last commit

--- fannie/reports/Store-Specific/FranklinCoop/BudgetSalesReport/budgetSales.js
+++ fannie/reports/Store-Specific/FranklinCoop/BudgetSalesReport/budgetSales.js
@@ -13,9 +13,7 @@ var budgetSales = (function ($) {
     	console.log(totalsChart);
     	var daily = [];
     	var xLabels = totalsChart[0];
-    	var line1 = [];
-    	var line2 = [];
-    	var line3 = [];
+    	var borderDash = [[10,5],[],[2,1]];
     	for (var i=1; i<totalCol; i++) {
         	var line = totalsChart[i];
         	console.log(line);
@@ -24,20 +22,18 @@ var budgetSales = (function ($) {
         }
         //var yData
         $('#reportTable1').after('<div class="col-sm-6 col-sm-offset-3"><canvas id="dailyCanvas"></canvas></div>');
-    	CoreChart.lineChart('dailyCanvas', xLabels, daily, dailyLabels);
+    	CoreChart.lineChart('dailyCanvas', xLabels, daily, dailyLabels, borderDash);
     };
 
     mod.chartAll = function(totalCol) {
-    	var dailyLabels = ["Budget","CY","PY"];
+    	var dailyLabels = ["Budget","This Year","Last Year"];
     	console.log(deptCharts);
     	for (var i = deptCharts.length - 1; i >= 0; i--) {
     		var chart = deptCharts[i];
     		console.log(chart);
     		var daily = [];
     		var xLabels = chart[0];
-    		var line1 = [];
-    		var line2 = [];
-    		var line3 = [];
+    		var borderDash = [[10,5],[],[2,1]];
     		for (var j=1; j<totalCol; j++) {
         		var line = chart[j];
         		console.log(line);
@@ -46,7 +42,7 @@ var budgetSales = (function ($) {
         	}
         	//var yData
         	$(`#reportTable${i+2}`).after(`<div class="col-sm-6 col-sm-offset-3"><canvas id="canvas${i}"></canvas></div>`);
-    		CoreChart.lineChart(`canvas${i}`, xLabels, daily, dailyLabels);
+    		CoreChart.lineChart(`canvas${i}`, xLabels, daily, dailyLabels, borderDash);
 
     	}
     }

8ec454e527253f610666732099580133f3e26087
roberski <rowan.oberski@gmail.com>
2018-11-21 10:12:46 -0500

    Produce Sign Layouts

--- fannie/classlib2.0/item/FpdfWithMultiCellCount.php
+++ fannie/classlib2.0/item/FpdfWithMultiCellCount.php
@@ -7,7 +7,206 @@ if (!class_exists('FPDF')) {
     include(dirname(__FILE__) . '/../../../src/fpdf/fpdf.php');
 }
 class FpdfWithMultiCellCount extends FPDF
-{
+{   
+
+        // Sets line style
+    // Parameters:
+    // - style: Line style. Array with keys among the following:
+    //   . width: Width of the line in user units
+    //   . cap: Type of cap to put on the line (butt, round, square). The difference between 'square' and 'butt' is that 'square' projects a flat end past the end of the line.
+    //   . join: miter, round or bevel
+    //   . dash: Dash pattern. Is 0 (without dash) or array with series of length values, which are the lengths of the on and off dashes.
+    //           For example: (2) represents 2 on, 2 off, 2 on , 2 off ...
+    //                        (2,1) is 2 on, 1 off, 2 on, 1 off.. etc
+    //   . phase: Modifier of the dash pattern which is used to shift the point at which the pattern starts
+    //   . color: Draw color. Array with components (red, green, blue)
+    function SetLineStyle($style) {
+        extract($style);
+        if (isset($width)) {
+            $width_prev = $this->LineWidth;
+            $this->SetLineWidth($width);
+            $this->LineWidth = $width_prev;
+        }
+        if (isset($cap)) {
+            $ca = array('butt' => 0, 'round'=> 1, 'square' => 2);
+            if (isset($ca[$cap]))
+                $this->_out($ca[$cap] . ' J');
+        }
+        if (isset($join)) {
+            $ja = array('miter' => 0, 'round' => 1, 'bevel' => 2);
+            if (isset($ja[$join]))
+                $this->_out($ja[$join] . ' j');
+        }
+        if (isset($dash)) {
+            $dash_string = '';
+            if ($dash) {
+                $tab = explode(',', $dash);
+                $dash_string = '';
+                foreach ($tab as $i => $v) {
+                    if ($i > 0)
+                        $dash_string .= ' ';
+                    $dash_string .= sprintf('%.2F', $v);
+                }
+            }
+            if (!isset($phase) || !$dash)
+                $phase = 0;
+            $this->_out(sprintf('[%s] %.2F d', $dash_string, $phase));
+        }
+        if (isset($color)) {
+            list($r, $g, $b) = $color;
+            $this->SetDrawColor($r, $g, $b);
+        }
+    }
+
+        // Draws a rounded rectangle
+    // Parameters:
+    // - x, y: Top left corner
+    // - w, h: Width and height
+    // - r: Radius of the rounded corners
+    // - round_corner: Draws rounded corner or not. String with a 0 (not rounded i-corner) or 1 (rounded i-corner) in i-position. Positions are, in order and begin to 0: top left, top right, bottom right and bottom left
+    // - style: Style of rectangle (draw and/or fill) (D, F, DF, FD)
+    // - border_style: Border style of rectangle. Array like for SetLineStyle
+    // - fill_color: Fill color. Array with components (red, green, blue)
+    function RoundedRect($x, $y, $w, $h, $r, $round_corner = '1111', $style = '', $border_style = null, $fill_color = null) {
+        if ('0000' == $round_corner) // Not rounded
+            $this->Rect($x, $y, $w, $h, $style, $border_style, $fill_color);
+        else { // Rounded
+            if (!(false === strpos($style, 'F')) && $fill_color) {
+                list($red, $g, $b) = $fill_color;
+                $this->SetFillColor($red, $g, $b);
+            }
+            switch ($style) {
+                case 'F':
+                    $border_style = null;
+                    $op = 'f';
+                    break;
+                case 'FD': case 'DF':
+                    $op = 'B';
+                    break;
+                default:
+                    $op = 'S';
+                    break;
+            }
+            if ($border_style)
+                $this->SetLineStyle($border_style);
+
+            $MyArc = 4 / 3 * (sqrt(2) - 1);
+
+            $this->_Point($x + $r, $y);
+            $xc = $x + $w - $r;
+            $yc = $y + $r;
+            $this->_Line($xc, $y);
+            if ($round_corner[0])
+                $this->_Curve($xc + ($r * $MyArc), $yc - $r, $xc + $r, $yc - ($r * $MyArc), $xc + $r, $yc);
+            else
+                $this->_Line($x + $w, $y);
+
+            $xc = $x + $w - $r ;
+            $yc = $y + $h - $r;
+            $this->_Line($x + $w, $yc);
+
+            if ($round_corner[1])
+                $this->_Curve($xc + $r, $yc + ($r * $MyArc), $xc + ($r * $MyArc), $yc + $r, $xc, $yc + $r);
+            else
+                $this->_Line($x + $w, $y + $h);
+
+            $xc = $x + $r;
+            $yc = $y + $h - $r;
+            $this->_Line($xc, $y + $h);
+            if ($round_corner[2])
+                $this->_Curve($xc - ($r * $MyArc), $yc + $r, $xc - $r, $yc + ($r * $MyArc), $xc - $r, $yc);
+            else
+                $this->_Line($x, $y + $h);
+
+            $xc = $x + $r;
+            $yc = $y + $r;
+            $this->_Line($x, $yc);
+            if ($round_corner[3])
+                $this->_Curve($xc - $r, $yc - ($r * $MyArc), $xc - ($r * $MyArc), $yc - $r, $xc, $yc - $r);
+            else {
+                $this->_Line($x, $y);
+                $this->_Line($x + $r, $y);
+            }
+            $this->_out($op);
+        }
+    }
+
+
+    // Draws a rectangle where the top edge is a Bézier curve
+    // Parameters:
+    // - x0, y0: Start point
+    // - x1, y1: Control point 1
+    // - x2, y2: Control point 2
+    // - x3, y3: End point of curve
+    // - w, h: Height and Width 
+    // - style: Style of rectangule (draw and/or fill: D, F, DF, FD)
+    // - line_style: Line style for curve. Array like for SetLineStyle
+    // - fill_color: Fill color. Array with components (red, green, blue)
+    function topCurveRect($x0, $y0, $x1, $y1, $x2, $y2, $x3, $y3,$w,$h, $style = '', $line_style = null, $fill_color = null) {
+        if (!(false === strpos($style, 'F')) && $fill_color) {
+            list($r, $g, $b) = $fill_color;
+            $this->SetFillColor($r, $g, $b);
+        }
+        switch ($style) {
+            case 'F':
+                $op = 'f';
+                $line_style = null;
+                break;
+            case 'FD': case 'DF':
+                $op = 'B';
+                break;
+            default:
+                $op = 'S';
+                break;
+        }
+        if ($line_style)
+            $this->SetLineStyle($line_style);
+
+
+        $this->_Point($x0,$y0);
+        $this->_Curve($x1,$y1,$x2,$y2,$x3,$y3);
+        //if ($h-$y3 == 0) {
+            $this->_Line($x0+$w,$y0+$h);
+        //}
+        $this->_Line($x0,$y0+$h);
+        $this->_Line($x0,$y0);
+       
+        $this->_out($op);
+    }
+
+    // Draws a Bézier curve (the Bézier curve is tangent to the line between the control points at either end of the curve)
+    // Parameters:
+    // - x0, y0: Start point
+    // - x1, y1: Control point 1
+    // - x2, y2: Control point 2
+    // - x3, y3: End point
+    // - style: Style of rectangule (draw and/or fill: D, F, DF, FD)
+    // - line_style: Line style for curve. Array like for SetLineStyle
+    // - fill_color: Fill color. Array with components (red, green, blue)
+    function Curve($x0, $y0, $x1, $y1, $x2, $y2, $x3, $y3, $style = '', $line_style = null, $fill_color = null) {
+        if (!(false === strpos($style, 'F')) && $fill_color) {
+            list($r, $g, $b) = $fill_color;
+            $this->SetFillColor($r, $g, $b);
+        }
+        switch ($style) {
+            case 'F':
+                $op = 'f';
+                $line_style = null;
+                break;
+            case 'FD': case 'DF':
+                $op = 'B';
+                break;
+            default:
+                $op = 'S';
+                break;
+        }
+        if ($line_style)
+            $this->SetLineStyle($line_style);
+
+        $this->_Point($x0, $y0);
+        $this->_Curve($x1, $y1, $x2, $y2, $x3, $y3);
+        $this->_out($op);
+    }
 
     //Cell with horizontal scaling if text is too wide
     function CellFit($w, $h=0, $txt='', $border=0, $ln=0, $align='', $fill=false, $link='', $scale=false, $force=true)
@@ -185,8 +384,33 @@ class FpdfWithMultiCellCount extends FPDF
         return $nl;
     }
 
+       /* PRIVATE METHODS */
+
+    // Sets a draw point
+    // Parameters:
+    // - x, y: Point
+    function _Point($x, $y) {
+        $this->_out(sprintf('%.2F %.2F m', $x * $this->k, ($this->h - $y) * $this->k));
+    }
+
+    // Draws a line from last draw point
+    // Parameters:
+    // - x, y: End point
+    function _Line($x, $y) {
+        $this->_out(sprintf('%.2F %.2F l', $x * $this->k, ($this->h - $y) * $this->k));
+    }
+
+    // Draws a Bézier curve from last draw point
+    // Parameters:
+    // - x1, y1: Control point 1
+    // - x2, y2: Control point 2
+    // - x3, y3: End point
+    function _Curve($x1, $y1, $x2, $y2, $x3, $y3) {
+        $this->_out(sprintf('%.2F %.2F %.2F %.2F %.2F %.2F c', $x1 * $this->k, ($this->h - $y1) * $this->k, $x2 * $this->k, ($this->h - $y2) * $this->k, $x3 * $this->k, ($this->h - $y3) * $this->k));
+    }
 
     }
+
 }
 
 ?>
---
+++ fannie/classlib2.0/item/signage/FCCProduce6upP.php
@@ -0,0 +1,342 @@
+<?php
+/*******************************************************************************
+
+    Copyright 2018 Franklin Community co-op
+
+    This file is part of CORE-POS.
+
+    IT CORE is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    IT CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in the file license.txt along with IT CORE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*********************************************************************************/
+namespace COREPOS\Fannie\API\item\signage;
+if (!class_exists('FpdfWithMultiCellCount')) {
+    include(dirname(__FILE__) . '/../FpdfWithMultiCellCount.php');
+}
+
+class FCCProduce6upP extends \COREPOS\Fannie\API\item\FannieSignage 
+{
+    protected $BIG_FONT = 29.37;
+    protected $MED_FONT = 9;
+    protected $SMALL_FONT = 7;
+
+    protected $fontHead = 'ModestoIOpenPrimary';
+    protected $font = 'ModestoOpenInlineFill';
+    protected $fontH = 'ModestoOpenInlineFillH';
+    protected $fontM = 'ModestoOpenInlineFillM';
+
+    protected $width = 288;
+    protected $height = 216;
+    protected $startX = 18;
+    protected $startY = 36;
+    protected $borderLineWidth=11;
+    protected $outerBorderWidth=7;
+    protected $innderBorderWidth=3;
+    public function drawPDF()
+    {
+        set_time_limit(660);
+        //define('FPDF_FONTPATH', dirname(__FILE__) . '/../../../src/fpdf/font/');
+        define('FPDF_FONTPATH',dirname(__FILE__) . '/../../../src/fpdf/font/proprietary/');
+        $pdf = new \FpdfWithMultiCellCount('P', 'pt', 'Letter');
+        $pdf->AddFont('ModestoIOpenPrimary','','ModestoIOpenPrimary.php');
+        $pdf->AddFont('ModestoOpenInlineFillH', '', 'ModestoOpen-InlineFillH.php');
+        $pdf->AddFont('ModestoOpenInlineFillM', '','ModestoOpen-InlineFillM.php');
+        $pdf->AddFont('ModestoOpenInlineFill', '', 'ModestoOpenInlineFill.php');
+        $pdf->SetMargins($this->startX, $this->startY, $this->startX);
+        $pdf->SetAutoPageBreak(false);
+        $pdf = $this->loadPluginFonts($pdf);
+        $pdf->SetFont($this->font, '', 16);
+
+        $data = $this->loadItems();
+        
+
+        $count = 0;
+        $sign = 0;
+        //fpdf draws the border rect is outside of the width this accounts for the extra size in placemnt.
+        //the fallowing two methods account for porder size when seting starting postions.
+        $xOffset = $this->width + $this->borderLineWidth/2 ;
+        $yOffset = $this->height + $this->borderLineWidth/2 ;
+        foreach ($data as $item) {
+            if ($count % 6 === 0) {
+                $pdf->AddPage();
+                $sign = 0;
+                //$file = dirname(__FILE__) . '/../../../item/images/MemberAlminac2x4BG.jpg';
+                //if (file_exists($file) && is_file($file)) {
+                //    $pdf->Image($file, 45, 55, 700);
+                //}
+            }
+
+
+            $row = floor($sign / 2);
+            $column = $sign % 2;
+            $info = $this->getExtraInfo($item['upc']);
+            $flags = $this->getProdFlags($item['upc']);
+            $y=$this->startY;
+            $x=$this->startX;
+            $textWidth = $this->width - ($this->width*.4) - 4;
+
+
+            //draw border
+            $pdf->SetDrawColor(118,185,181);
+            $pdf->SetLineWidth($this->outerBorderWidth);
+            $y += $yOffset*$row;
+            $x += $xOffset*$column;
+            $pdf->Rect($x, $y, $this->width, $this->height);
+            $pdf->SetDrawColor(30, 77, 44);
+            $pdf->SetLineWidth($this->innderBorderWidth);
+            $y += $this->outerBorderWidth/2;
+            $x += $this->outerBorderWidth/2;
+            $pdf->Rect($x, $y, $this->width-$this->outerBorderWidth, $this->height-$this->outerBorderWidth);
+            $pdf->SetLineWidth(1);
+            
+
+
+
+            if ($flags['Local']&&$flags['Organic']) {
+                //LOCAL
+                $start_x = ($xOffset*$column) + $this->startX+($this->borderLineWidth/2)-1;
+                $start_y = ($yOffset*$row) + $this->startY-($this->borderLineWidth/2) + $this->height -59.14;
+                $end_x = ($xOffset*$column) + $this->width+1+$this->startX-($this->borderLineWidth/2);
+                $end_y = ($yOffset*$row) + $this->height+$this->startY-($this->borderLineWidth/2) -20;
+                $eq_x1 = $start_x+200;
+                $eq_y1 = $start_y;
+                $eq_x2 = $end_x;
+                $eq_y2 = $end_y;
+                $w = $this->width-9;
+                $h = 60.14;
+                $pdf->topCurveRect($start_x,$start_y,$eq_x1,$eq_y1,$eq_x2,$eq_y2,$end_x,$end_y,$w, $h,'FD', null, array(151,174,103));
+                $fontSize = 28;
+                $pdf->SetTextColor(30, 77, 44);
+                $pdf->SetFont('ModestoOpenInlineFillH', '', $fontSize);
+                $pdf->SetXY($start_x,$start_y+2);
+                $pdf->CellFit($this->width-170, $fontSize, 'LOCAL',0,0,'L');
+                // ORGANIC
+                $start_x = ($xOffset*$column) + $this->startX+($this->borderLineWidth/2)-1;
+                $start_y = ($yOffset*$row) + $this->startY-($this->borderLineWidth/2) + $this->height -30.4;
+                $end_x = ($xOffset*$column) + $this->width+$this->startX-($this->borderLineWidth/2);
+                $end_y = ($yOffset*$row) + $this->height+$this->startY-($this->borderLineWidth/2)-5;
+                $eq_x1 = $start_x+200;
+                $eq_y1 = $start_y-5;
+                $eq_x2 = $end_x;
+                $eq_y2 = $end_y;
+                $w = $this->width-8;
+                $h = 31.4;
+                $pdf->topCurveRect($start_x,$start_y,$eq_x1,$eq_y1,$eq_x2,$eq_y2,$end_x,$end_y,$w, $h,'FD', null, array(30, 77, 44));
+                $fontSize = 28;
+                $pdf->SetFont('ModestoOpenInlineFillH', '', $fontSize);
+                $pdf->SetTextColor(255,255,255);
+                $pdf->SetXY($start_x,$start_y+5);
+                $pdf->CellFit($w-130, $fontSize, 'ORGANIC',0,0,'L');
+            } elseif ($flags['Local']) {
+                //LOCAL .43 inch 31pt
+                $start_x = ($xOffset*$column) + $this->startX+($this->borderLineWidth/2)-1;
+                $start_y = ($yOffset*$row) + $this->startY-($this->borderLineWidth/2) + $this->height -31.4;
+                $end_x = ($xOffset*$column) + $this->width+$this->startX-($this->borderLineWidth/2);
+                $end_y = ($yOffset*$row) + $this->height+$this->startY-($this->borderLineWidth/2)+1;
+                $pdf->topCurveRect($start_x,$start_y,$start_x+200,$start_y,$end_x,$end_y,$end_x,$end_y,$this->width-8, 32.4,'FD', null, array(151,174,103));
+                $fontSize = 28;
+                $pdf->SetTextColor(30, 77, 44);
+                $pdf->SetFont('ModestoOpenInlineFillH', '', $fontSize);
+                $pdf->SetXY($start_x,$start_y+4);
+                $pdf->Cell($this->width-170, $fontSize, 'LOCAL',0,0,'L');
+            } elseif ($flags['Organic']) {
+                // ORGANIC
+                $start_x = ($xOffset*$column) + $this->startX+($this->borderLineWidth/2)-1;
+                $start_y = ($yOffset*$row) + $this->startY-($this->borderLineWidth/2) + $this->height -32.4;
+                $end_x = ($xOffset*$column) + $this->width+$this->startX-($this->borderLineWidth/2);
+                $end_y = ($yOffset*$row) + $this->height+$this->startY-($this->borderLineWidth/2)+1;
+                $pdf->topCurveRect($start_x,$start_y,$start_x+200,$start_y,$end_x,$end_y,$end_x,$end_y,$this->width-8, 33.4,'FD', null, array(30, 77, 44));
+                $fontSize = 28;
+                $w = $this->width-8;
+                $pdf->SetFont('ModestoOpenInlineFillH', '', $fontSize);
+                $pdf->SetTextColor(255,255,255);
+                $pdf->SetXY($start_x,$start_y+7);
+                $pdf->CellFit($w-130, $fontSize, 'ORGANIC',0,0,'L');
+            }
+            
+            $pdf->SetTextColor(30, 77, 44);
+            //PLU or UPC
+            $plu = $this->barcodeText($item['upc'], strlen($item['upc']));
+            $fontSize = 10;
+            $x += 0;
+            $y += 3;
+            $w = $this->width;
+            $h = $fontSize;
+            $pdf->SetFont('ModestoOpenInlineFillH','',$fontSize);
+            $pdf->SetFontSize($fontSize);
+            $pdf->SetXY($x, $y);
+            $pdf->Cell($w,$h, $plu, 0,0, 'L');
+            //Origin
+            $origin = ($item['originName'] == '') ? 'ORIGIN:                       ' : $item['originName'] ;
+            $x += 0;
+            $y += 0;
+            $w = $this->width - $this->borderLineWidth*2 -9;
+            $h = $fontSize;
+            //$pdf->SetFont($this->font,'',7);
+            //$pdf->SetFontSize($this->SMALL_FONT);
+            $pdf->SetXY($x, $y);
+            $pdf->Cell($w, $h, $origin, 0,0, 'R');
+
+
+            //Brand
+            $brand = ($item['brand']=='') ? 'FRESH' : $item['brand'] ;
+            $x = $this->startX + $xOffset*$column + $this->outerBorderWidth/2;
+            $y += $fontSize +10;
+            $fontSize = 14;
+            $w = $this->width - $this->outerBorderWidth;
+            $h = $fontSize;
+            $spacing = 5;
+            $pdf->SetFont('ModestoOpenInlineFillH','',$fontSize);
+            $pdf->SetXY($x, $y);            
+            
+            $pdf->Cell($w, $h, $brand, 0, 0, 'C');
+            //Description
+            $y += $fontSize + 1.5;
+            $fontSize = 22;
+            $h = $fontSize;
+            $pdf->SetFont('ModestoOpenInlineFillH','',$fontSize);
+            $pdf->SetXY($x, $y);
+            $lines = $pdf->MultiCellRet($w, $h, $item['description'],0, 'C');
+            $blankSpace = ($lines==1) ? $fontSize : 0;
+            // Vendor/Farm
+
+            //price
+            $price = sprintf('$%.2f', $item['normal_price']);
+            //$x -= 10;
+            $y += $fontSize + 30; //space bewtten top and start of first element.
+            $fontSize = 46;
+            $h = $fontSize;
+            $pdf->SetFont('ModestoOpenInlineFillH','',$fontSize);
+            $pdf->SetXY($x, $y);
+            $pdf->Cell($w, $h, $price, 0, 0, 'C');
+
+            //units
+            $units = '';
+            if ($info['scale'] == 1) {
+                $units = 'per Pound';
+            } else {
+                $units = 'per Each';
+            }
+            //$x += 10;
+            $y += $fontSize; //space bewtten top and start of first element.
+            $fontSize = 14;
+            $h = $fontSize;
+            $pdf->SetFont('ModestoOpenInlineFillH','',$fontSize);
+            $pdf->SetXY($x, $y);
+            $pdf->Cell($w, $h, $units, 0, 0, 'C');
+
+
+
+            //desciption
+
+            //$pdf->Cell($textWidth/2, $fontSize, sprintf('REG. $%.2f', $info['normal_price']), 0,0, 'L');
+            //MultiCellRet
+            
+            //$pdf->Ln(1);
+            //brand
+
+
+            //reg price
+
+            //$info = $this->getExtraInfo($item['upc']);
+            //$pdf->SetX(10 + ($this->width*$column));
+            //$pdf->MultiCell($this->width-5, 4, str_replace('<br />', "\n", $info['long_text']), 0, 'L');
+
+            //$pdf->SetX(10 + ($this->width*$column));
+            //$pdf->SetFont($this->font, 'B', $this->MED_FONT);
+            //$pdf->Cell($this->width-5, 9, $item['originName'], 0, 1, 'R');
+/*
+            $pdf->SetFont($this->font, '', $this->MED_FONT);
+            $pdf->SetXY(10 + ($this->width*$column), 5 + ($row*$this->height) + $this->height - 10 - 1);
+            $pdf->Cell($this->width-5, 7, '', 1); // blank box
+            $pdf->SetXY(15 + ($this->width*$column), 5 + ($row*$this->height) + $this->height - 10);
+            $pdf->Cell(20, 5, 'PLU#', 0, 0, 'L');
+            $pdf->SetFont($this->font, 'B', $this->MED_FONT);
+            $pdf->Cell(30, 5, ltrim($item['upc'], '0'), 0, 0, 'L');
+            $pdf->SetFont($this->font, '', $this->MED_FONT);
+            $pdf->Cell(20, 5, sprintf('$%.2f/lb.', $item['normal_price']), 0, 0, 'L');
+            $file = dirname(__FILE__) . '/../../../item/images/nutrition-facts/' . $info['nutritionFacts'];
+            if (file_exists($file) && is_file($file)) {
+                $pdf->Image($file, 10+($this->width*$column), 11.5 + ($row*$this->height) + 52, 88.9);
+            } */
+
+            $count++;
+            $sign++;
+        }
+
+        $pdf->Output('CakeOfTheMonth6upP.pdf', 'I');
+        set_time_limit(30);
+    }
+
+    protected function getProdFlags($upc) {
+        $dbc = \FannieDB::get(\FannieConfig::config('OP_DB'));
+        $query = "
+            SELECT f.description,
+                f.bit_number,
+                (1<<(f.bit_number-1)) & p.numflag AS flagIsSet
+            FROM products AS p, 
+                prodFlags AS f
+            WHERE p.upc=?
+                " . (\FannieConfig::config('STORE_MODE') == 'HQ' ? ' AND p.store_id=? ' : '') . "
+                AND f.active=1";
+        $args = array($upc);
+        if (\FannieConfig::config('STORE_MODE') == 'HQ') {
+            $args[] = \qFannieConfig::config('STORE_ID');
+        }
+        $prep = $dbc->prepare($query);
+        $res = $dbc->execute($prep,$args);
+        
+        if ($dbc->numRows($res) == 0){
+            // item does not exist
+            $prep = $dbc->prepare('
+                SELECT f.description,
+                    f.bit_number,
+                    0 AS flagIsSet
+                FROM prodFlags AS f
+                WHERE f.active=1');
+            $res = $dbc->execute($prep);
+        }//please use the order  "Local, Organic, NONGMO, Gluten Free
+        //please use the order  "Local, Organic, NONGMO, Gluten Free
+        $flags = array('Local'=> false, 'Organic' => false, 'Non_GMO' => false, 'Gluten Free'=>false);
+        
+        while($info = $dbc->fetchRow($res)){
+                $flags[$info['description']] = $info['flagIsSet'];
+       }
+       $showLocal = $flags['Local'];
+       $showOrganic = $flags['Organic'];
+       $showNONGMO = $flags['Non_GMO'];
+       $showGlutenFree = $flags['Gluten Free'];
+
+       return $flags; 
+    }
+
+    protected function getExtraInfo($upc)
+    {
+        $dbc = \FannieDB::get(\FannieConfig::config('OP_DB'));
+        $prep = $dbc->prepare('SELECT * FROM products WHERE upc=?');
+        return $dbc->getRow($prep, array($upc));
+    }
+
+    private function barcodeText($barcode,$len)
+    {
+        if($len ==12) {
+            $barText = 'UPC: '.substr($barcode,0,2)."-".substr($barcode,2,5)."-".substr($barcode,7,5)."-".substr($barcode,12);
+            $len+=3;
+        } else {
+            $barText = 'PLU: '.ltrim($barcode,'0');
+        }
+        return $barText;
+    }
+}
+

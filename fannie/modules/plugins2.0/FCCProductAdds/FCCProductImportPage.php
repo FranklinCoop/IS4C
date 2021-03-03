e535f6c7a41a650eb65bf73dba812e1f0000858b
Rowan Oberski <rowan@Rowans-MacBook-Pro.local>
2021-03-03 11:47:42 -0500

    Another stupid error = instead of ==

--- fannie/modules/plugins2.0/FCCProductAdds/FCCProductImportPage.php
+++ fannie/modules/plugins2.0/FCCProductAdds/FCCProductImportPage.php
@@ -204,7 +204,7 @@ class FCCProductImportPage extends \COREPOS\Fannie\API\FannieUploadPage
             $model->upc($upc);
             $model->store_id(1);
             $exists = $model->load();
-            if ($upc = 'upc') {
+            if ($upc == 'upc') {
                 continue; //skip first line
             }
             $model->description($desc);

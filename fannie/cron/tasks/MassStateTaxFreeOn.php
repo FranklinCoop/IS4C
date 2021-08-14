016c0a4c9cfea417baa8249d1493189a40d5e6df
Rowan Oberski <rowan@Rowans-MacBook-Pro.local>
2021-08-14 13:54:44 -0400

    Fix to mass tax on

--- fannie/cron/tasks/MassStateTaxFreeOn.php
+++ fannie/cron/tasks/MassStateTaxFreeOn.php
@@ -68,7 +68,7 @@ Make sure to set MassStateTaxFreeOff to run to reenable taxes.';
             if (isset($FANNIE_LANES[$i]['offline']) && $FANNIE_LANES[$i]['offline']) {
                 continue;
             }
-            $lane = $FANNIE_LANES($i);
+            $lane = $FANNIE_LANES[$i];
 		    $dbc->addConnection($lane['host'],$lane['type'],$lane['trans'],$lane['user'],$lane['pw']);
 		    if ($dbc->connections[$lane['trans']] === False){
 		        echo cron_msg('Cannot connect to '.$lane['host']);

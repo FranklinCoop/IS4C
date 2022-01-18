c1a6174bed23b2d37455c82b1c67725ebfa84fec
Rowan Oberski <rowan@Rowans-MacBook-Pro.local>
2022-01-18 09:36:16 -0500

    bottle deposit save issue.

--- fannie/item/modules/BaseItemModule.php
+++ fannie/item/modules/BaseItemModule.php
@@ -765,7 +765,7 @@ HTML;
         </td>
         <th class="small text-right">Deposit</th>
         <td colspan="2">
-            <input type="text" name="deposit-upc[]" class="form-control input-sm syncable-input"
+            <input type="text" name="deposit-upc" class="form-control input-sm syncable-input"
                 value="{$deposit}" placeholder="Deposit Item PLU/UPC"
                 />
         </td>

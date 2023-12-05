a84bc916381671fcea8479b9de51c2fd19524a1d
Rowan Oberski <rowan.oberski@franklincommunity.coop>
2023-12-05 12:48:35 -0500

    Price Checker Single Page with Configurable printers, forgot to upload the page for configs

--- fannie/modules/plugins2.0/PriceCheckTablet/PriceCheckTablet.php
+++ fannie/modules/plugins2.0/PriceCheckTablet/PriceCheckTablet.php
@@ -5,10 +5,17 @@ use COREPOS\Fannie\API\FanniePlugin;
 class PriceCheckTablet extends FanniePlugin
 {
     public $plugin_settings = array(
-    'T1PrintIP' => array('default'=>'','label'=>'Printer IP/Host',
-            'description'=>'IP or hostname for printer'),
+            'tabletSerials' => array('default'=>'','label'=>'Serial ID list of tablets',
+            'description'=>'Comma seprated list of serials for tablets'),
+            'printIPs' => array('default'=>'','label'=>'List of IP/Host for printers',
+            'description'=>'A comma sperated list of printer IPs 1 to 1 with the tablet serail
+                            You want to print from. e.g 192.168.1.X, 0, 192.168.1.Y
+                            You can have tablets print to the same printer by adding the ip for
+                            each printer. 0 will not print or display the print button.'),
     );
 
-    public $plugin_description = 'Tool to build, suspend, & print small orders';
+    public $plugin_description = 'Price Check page that can print to netowrked epson receipt printers based on tablet serial number
+    requires a tablet that can be made to send its serial to a webpage. I\'m using elotouch 10 inch tablets.
+    A tablet with no serel IP combantion set will have no print option.';
 }
 

0d2c9433e89309f5037d50fb76f439da80528134
Rowan Oberski <rowan.oberski@franklincommunity.coop>
2023-12-06 10:54:24 -0500

    Doing this the cludgy way now.

---
+++ fannie/modules/plugins2.0/PriceCheckTablet/priceCheckTabletGFMFP.js
@@ -0,0 +1,34 @@
+
+var priceCheckTabletGFMFP = (function ($) {
+
+    var mod = {};
+
+    mod.showDefault = function() {
+        $('#pc-results').html('<div class="alert alert-info h1">Please Scan Item</div>');
+    };
+
+    var timeout;
+
+    mod.search = function() {
+        $.ajax({
+            url: 'PriceCheckTabletPageGFMFP.php',
+            data: $('#pc-upc').serialize()
+        }).done(function (resp) {
+            $('#pc-results').html(resp);
+        }).always(function() {
+            $('#pc-upc').val('');
+            $('#pc-upc').focus();
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+            timeout = setTimeout(mod.showDefault, 15000);
+        });
+    };
+
+    return mod;
+
+}(jQuery));
+// this will return the focus to the input for any keyboard even so users can't unselect the field.
+document.onkeydown = function(evt) {
+    $('#pc-upc').focus();    
+};
